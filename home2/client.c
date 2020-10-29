e7822c8626f0184c213d91885ffef5433b311a3a
sultanyaril <sultanyaril@gmail.com>
2020-10-29 20:20:23 +0600

    add 1 & 2

---
+++ home2/client.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+enum errors {
+    OK,
+    ERR_INCORRECT_ARGS,
+    ERR_SOCKET,
+    ERR_CONNECT
+};
+
+char *get_word(int *size) {
+    char *answ = NULL;
+    char c = getchar();
+    int cnt = 0;
+    while (c != '\n' && c != ' ') {
+        cnt++;
+        answ = realloc(answ, (cnt + 1) * sizeof(char));
+        answ[cnt - 1] = c;
+        c = getchar();
+    }
+    *size = cnt;
+    return answ;
+}
+
+int init_socket(const char *ip, int port) {
+    //open socket, result is socket descriptor
+    int server_socket = socket(PF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        perror("Fail: open socket");
+        _exit(ERR_SOCKET);
+    }
+
+    //prepare server address
+    struct hostent *host = gethostbyname(ip);
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port);
+    memcpy(&server_address.sin_addr, host -> h_addr_list[0], sizeof(server_address));
+
+    //connection
+    struct sockaddr_in sin;
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons(port);
+    memcpy(&sin.sin_addr, host->h_addr_list[0], sizeof(sin.sin_addr));
+    if (connect(server_socket, (struct sockaddr*) &sin, sizeof(sin)) < 0) {
+        perror("Fail: connect");
+        _exit(ERR_CONNECT);
+    }
+    return server_socket;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        puts("Incorrect args.");
+        puts("./client <ip> <port>");
+        puts("Example:");
+        puts("./client 127.0.0.1 5000");
+        return ERR_INCORRECT_ARGS;
+    }
+    char *ip = argv[1];
+    int port = atoi(argv[2]);
+    int server = init_socket(ip, port);
+    char *word;
+    int size_w;
+    for(word = get_word(&size_w); strcmp(word, "exit") && strcmp(word, "quit"); word = get_word(&size_w)) {
+            write(server, word, size_w);
+            printf("Send word: ");
+            puts(word);
+    }
+    char ch = '.';
+    write(server, &ch, 1);
+    close(server);
+    return OK;
+}
---
+++ home2/server.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
+enum errors {
+    OK,
+    ERR_INCORRECT_ARGS,
+    ERR_SOCKET,
+    ERR_SETSOCKETOPT,
+    ERR_BIND,
+    ERR_LISTEN
+};
+
+int init_socket(int port) {
+    //open socket, return socket descriptor
+    int server_socket = socket(PF_INET, SOCK_STREAM, 0);
+    if (server_socket < 0) {
+        perror("Fail: open socket");
+        _exit(ERR_SOCKET);
+    }
+ 
+    //set socket option
+    int socket_option = 1;
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &socket_option, sizeof(socket_option));
+    if (server_socket < 0) {
+        perror("Fail: set socket options");
+        _exit(ERR_SETSOCKETOPT);
+    }
+
+    //set socket address
+    struct sockaddr_in server_address;
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(port);
+    server_address.sin_addr.s_addr = INADDR_ANY;
+    if (bind(server_socket, (struct sockaddr *) &server_address, sizeof(server_address)) < 0) {
+        perror("Fail: bind socket address");
+        _exit(ERR_BIND);
+    }
+
+    //listen mode start
+    if (listen(server_socket, 5) < 0) {
+        perror("Fail: bind socket address");
+        _exit(ERR_LISTEN);
+    }
+    return server_socket;
+}
+
+
+int main(int argc, char** argv) {
+    if (argc != 3) {
+        puts("Incorrect args.");
+        puts("./server <port> <client_numb>");
+        puts("Example:");
+        puts("./server 5000 2");
+        return ERR_INCORRECT_ARGS;
+    }
+    int client_numb = atoi(argv[2]);
+    int port = atoi(argv[1]);
+    int server_socket = init_socket(port);
+    puts("Wait for connection");
+    struct sockaddr_in client_address;
+    socklen_t size;
+    int *client_socket = malloc(client_numb * sizeof(int));
+    for (int i = 0; i < client_numb; i++) {
+        client_socket[i] = accept(server_socket, 
+                                (struct sockaddr *) &client_address,
+                                &size);
+        printf("connected: %s %d\n", inet_ntoa(client_address.sin_addr),
+                                ntohs(client_address.sin_port));
+    }
+    char ch;
+    pid_t *pid = malloc(client_numb * sizeof(pid_t));
+    for (int i = 0; i < client_numb; i++) {
+        pid[i] = fork();
+        if (pid[i] == 0) {
+            while (ch != '.') {
+                read(client_socket[i], &ch, 1);
+                printf("%d: ", i + 1);
+                putchar(ch);
+                puts("");
+            }
+            _exit(1);
+        }
+    }
+    for (int i = 0; i < client_numb; i++) {
+        waitpid(pid[i], 0, 0);
+        close(client_socket[i]);
+    }
+    free(client_socket);
+    return OK;
+}
