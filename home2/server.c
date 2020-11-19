62e457aec696109573d5fce3d7e522a2671e62dd
sultanyaril <sultanyaril@gmail.com>
2020-11-20 01:09:54 +0600

    warnings, makefile, bin

--- home2/server.c
+++ home2/server.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <sys/wait.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
--- home3/server.c
+++ home3/server.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <sys/wait.h>
 #include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
